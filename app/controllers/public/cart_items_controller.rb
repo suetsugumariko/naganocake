72e2cd80f6023d2670462fd080c4ecb325ed294b
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-03-14 13:55:24 +0000

    [add]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,15 +14,20 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.new(cart_item_params)
+    #カート内の同じ種類の商品をまとめて表示する
+    #すでにカートにある商品がさらに追加されたときには、カートにある商品の数量を変更してupdate
+    #すでにカートにある商品の情報を知るため、カートの商品を全てもってくる
+    @cart_items = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+       if cart_item.item_id == @cart_item.item_id
+         #すでにカートに入れている数量と新しく追加した数量を足した数量をnew_quanitityとしてupdate
+          new_amount = cart_item.amount + @cart_item.amount
+          cart_item.update_attribute(:amount, new_amount)
+          @cart_item.delete
+       end
+    end
     @cart_item.save
-    redirect_to cart_items_path
-    #@cart_item = current_customer.cart_item.build(cart_item_params)
-    #@cart_items = current_customer.cart_items.all
-    #@cart_items.each do |cart_item|
-   #if cart_item.item_id == @cart_item.item_id
-
-   #end
-  #end
+    redirect_to :cart_items
   end
 
 
