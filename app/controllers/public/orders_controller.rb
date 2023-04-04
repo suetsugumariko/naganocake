7d66f82c7f4bcd7611406a93c45759d8b5024b49
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-04-04 15:20:09 +0000

    [add]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -20,8 +20,9 @@ class Public::OrdersController < ApplicationController
 
   def create
     order = Order.new(order_params)
+    @order.customer_id = current_customer.id
     order.save
-    redirect_to '  /orders/confirm'
+    redirect_to orders_complete_path
   end
 
   def index
