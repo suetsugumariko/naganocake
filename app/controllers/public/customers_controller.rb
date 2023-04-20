49d351b7d0caf979a0de61eecce787cbc025e3ba
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-04-20 14:18:31 +0000

    [add]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,18 +8,26 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    customer = current_customer
+    customer.update(customer_params)
+    redirect_to customers_my_page_path(customer.id)
   end
 
   def unsubscribe
   end
 
   def withdraw
+    @customer = current_customer
+    @customer.update(is_valid: false)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to  root_path(customer.id)
   end
 
 
   private
   def customer_params
-    params.require(:customer).permit(:name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
   end
 
 
