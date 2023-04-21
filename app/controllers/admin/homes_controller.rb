e945f8a8111cf50be0b2d22e054da0fabba213fa
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-04-21 10:11:19 +0000

    [add]

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,4 +2,22 @@ class Admin::HomesController < ApplicationController
   def top
     @orders = Order.all
   end
+
+
+
+
+
+
+
+
+
+   private
+
+  def homes_params
+    params.require(:homes).permit(:created_at, :name, :amount, :status)
+  end
+
+
+
+
 end
