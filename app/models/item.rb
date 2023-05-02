6017782a59117b90f546da28a080d6937f96d821
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-05-02 14:41:36 +0000

    [add]

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
  has_one_attached :image
  has_many :cart_items, dependent: :destroy
  belongs_to :genre
-
+ has_many :order_details, dependent: :destroy
  def with_tax_price
      (price * 1.1).floor
  end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
 
   enum payment_method: { credit_card: 0, transfer:1 }
 
+  has_many :order_details,dependent: :destroy
+
 end
