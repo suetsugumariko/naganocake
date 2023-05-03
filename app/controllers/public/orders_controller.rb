65e963ef42e171bc488ac44ab2a68d43ab39f3b6
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-05-03 16:16:06 +0000

    [add]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,4 +1,6 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
+
   def new
     @order = Order.new
   end
@@ -23,6 +25,16 @@ class Public::OrdersController < ApplicationController
     @order.customer_id = current_customer.id
     @order.status = 0
     @order.save
+    @cart_item = current_customer.cart_items.all
+    @cart_item.each do |cart_item|
+    @order_details = OrderDetail.new
+    @order_details.order_id = @order.id
+    @order_details.item_id = cart_item.item.id
+    @order_details.price = cart_item.item.price
+    @order_details.amount = cart_item.amount
+    @order_details.making_status = 0
+    @order_details.save
+    end
     current_customer.cart_items.destroy_all
     redirect_to orders_complete_path
   end
